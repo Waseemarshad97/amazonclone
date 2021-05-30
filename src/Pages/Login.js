8b351835b1eda56c607d7ef8d3700862f5d4e3c8
akbar ali <akkuakbar97@gmail.com>
2021-05-31 00:21:41 +0530

    new commit

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,31 +4,31 @@ import '../index.css';
 function Login() {
   return (
     <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-4">
+      <div className="row justify-content-center">
+        <div className="col-4">
           <img></img>
           <div className="loginContainer">
-          <h3>Sign In</h3>
-          <p><strong>Email or mobile phone number</strong></p>
-          <input></input>
-          <button>Continue</button>
-          <p>By continuing, you agree to amazon's <span>Conditions of use</span> and Privacy's Notice.</p>
-          <a>Need Help?</a>
-        </div>
-        <div className="newText">New to Amazon</div>
-      <div>
-        <button>
-          create your Amazon Account
+            <h3>Sign In</h3>
+            <p><strong>Email or mobile phone number</strong></p>
+            <input></input>
+            <button>Continue</button>
+            <p>By continuing, you agree to amazon's <span>Conditions of use</span> and Privacy's Notice.</p>
+            <a>Need Help?</a>
+          </div>
+          <div className="newText">New to Amazon</div>
+          <div>
+            <button>
+              create your Amazon Account
         </button>
-      </div>
-      </div>
-      
-      </div>
-       
+          </div>
+        </div>
 
-     
+      </div>
+
+
+
     </div>
-        );
+  );
 }
 
 export default Login;
