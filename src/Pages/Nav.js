6265731bdfbcce0342b8158ac2c80472904e0993
Abdul Salim <>
2021-06-07 13:10:45 +0530

    Cart design

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -36,66 +36,56 @@ const Nav = () => {
                 </div>
             </div>
             <Modal show={showModal} onHide={() => setShowModal(false)} scrollable>
-            <Modal.Header>
-                <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
+            {/* <Modal.Header>
+                <Modal.Title>Shopping Cart</Modal.Title>
+            </Modal.Header> */}
             <Modal.Body>
-                <div className="row">
+                <div className="row cart">
                     <div className="col-12">
+                        <h3 className="text-center mb-3 border-bottom">Shopping Cart</h3>
                         <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
+                            <img className="col-3 product-image"
+                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
+                                alt="name"
+                            />
+                            <div className="col-9 product-details">
                                 <strong>Name</strong>
                                 <p>details of the products get displayed here</p>
                                 <strong>Price</strong>
                             </div>
-                            <button className="btn">Add to Cart</button>
+                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
                         </div>
                         <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
+                            <img className="col-3 product-image"
+                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
+                                alt="name"
+                            />
+                            <div className="col-9 product-details">
                                 <strong>Name</strong>
                                 <p>details of the products get displayed here</p>
                                 <strong>Price</strong>
                             </div>
-                            <button className="btn">Add to Cart</button>
+                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
                         </div>
                         <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
+                            <img className="col-3 product-image"
+                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
+                                alt="name"
+                            />
+                            <div className="col-9 product-details">
                                 <strong>Name</strong>
                                 <p>details of the products get displayed here</p>
                                 <strong>Price</strong>
                             </div>
-                            <button className="btn">Add to Cart</button>
+                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
                         </div>
-                        <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn">Add to Cart</button>
-                        </div>
-                        <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn">Add to Cart</button>
-                        </div>
-                        <div className="cart-container">
-                            <img className="product-image" src="" alt="name" />
-                            <div className="product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn">Add to Cart</button>
-                        </div>
+                    </div>
+                    <h6 className="mt-3 ml-5">Subtotal ( 3 Item ) : <strong>1211121</strong></h6>
+                    <div className="col-12 d-flex justify-content-center mt-3">
+                        <button className="btn loginbtn mb-2">Proceed to Buy</button>
+                    </div>
+                    <div className="col-12">
+                        <button className="btn btn-sm btn-block btn-danger mb-4">Remove All</button>
                     </div>
                 </div>
             </Modal.Body>
