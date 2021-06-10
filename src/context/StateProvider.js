3370d1c6bdaeb93ef034af315f9799073b50c273
akbar ali <akkuakbar97@gmail.com>
2021-06-10 21:14:07 +0530

    adds contextApi functionality

---
+++ src/context/StateProvider.js
@@ -0,0 +1,8 @@
+import React, { createContext, useReducer } from 'react';
+export const StateContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
---
+++ src/context/reducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+    user: null,
+    cart: [],
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TO_CART':
+            return {
+            ...state,
+            cart: [...state.cart, action.item]
+        }
+        case 'REMOVE_FROM_CART':
+            return {
+                ...state,
+            }
+        default:
+            return state;
+    }
+}
+export default reducer;
