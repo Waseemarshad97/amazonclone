50927c4bfe4ec64171a261ec48d687f8ba315969
akbar ali <akkuakbar97@gmail.com>
2021-06-10 21:19:35 +0530

    removes cart from nav

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -3,7 +3,7 @@ import '../styles/Nav.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
-import { Modal, ModalBody } from 'react-bootstrap';
+import CartModal from '../Component/CartModal';
 
 const Nav = () => {
     const [showModal, setShowModal] = useState(false);
@@ -35,63 +35,7 @@ const Nav = () => {
                     <span className="cart-count">2</span>
                 </div>
             </div>
-            <Modal size="lg" show={showModal} onHide={() => setShowModal(false)} scrollable>
-            {/* <Modal.Header>
-                <Modal.Title>Shopping Cart</Modal.Title>
-            </Modal.Header> */}
-            <Modal.Body>
-                <div className="row cart">
-                    <div className="col-12">
-                        <h3 className="text-center mb-3 border-bottom">Shopping Cart</h3>
-                        <div className="cart-container">
-                            <img className="col-3 product-image"
-                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
-                                alt="name"
-                            />
-                            <div className="col-9 product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
-                        </div>
-                        <div className="cart-container">
-                            <img className="col-3 product-image"
-                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
-                                alt="name"
-                            />
-                            <div className="col-9 product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
-                        </div>
-                        <div className="cart-container">
-                            <img className="col-3 product-image"
-                                src="https://firebasestorage.googleapis.com/v0/b/reactworkshop-dfd9c.appspot.com/o/images%2F71ZOtNdaZCL._AC_SL1500_.jpg?alt=media&token=8ba01665-325b-4e1d-8daf-e7cc9e39c48a"
-                                alt="name"
-                            />
-                            <div className="col-9 product-details">
-                                <strong>Name</strong>
-                                <p>details of the products get displayed here</p>
-                                <strong>Price</strong>
-                            </div>
-                            <button className="btn-sm btn-danger mt-2">Remove Item</button>
-                        </div>
-                    </div>
-                    <h6 className="mt-3 ml-5">Subtotal ( 3 Item ) : <strong>1211121</strong></h6>
-                    <div className="col-12 d-flex justify-content-center mt-3">
-                        <button className="btn loginbtn mb-2">Proceed to Buy</button>
-                    </div>
-                    <div className="col-12">
-                        <button className="btn btn-sm btn-block btn-danger mb-4">Remove All</button>
-                    </div>
-                </div>
-            </Modal.Body>
-
-            </Modal> 
-
+            <CartModal show={showModal} onHide={() => setShowModal(false)} />
         </div>
     )
 }
